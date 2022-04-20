6fcbb963ca70a8644e4765494c9ec3b9eb6a2264
jaramillo <jaramillito098x@gmail.com>
2022-04-20 17:28:27 -0500

    index.html

---
+++ controlador.js
@@ -0,0 +1,9 @@
+let numeros=Array(1,2,3,4,5,6,7,8,9,10)
+console.log (numeros)
+let nombres=Array("Jose","Aristi","Isabela","Luis","Sara","Mateo","Juan","Jacobo","Camila","Juan")
+console.log(nombres)
+let edades=Array(1,2,3,4,5,6,7,8,9,10)
+console.log(edades)
+for(let i=0; i<nombres.length; i++){
+console.log(nombres[i])
+}
